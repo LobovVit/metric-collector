29c3bf0d59faa7c80eb039c2d85908fc463d1e67
vitaly <lobov.vitaliy@gmail.com>
2024-01-02 00:52:39 +0300

    iter4 fix "add http"

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -5,6 +5,10 @@ import (
 	"github.com/LobovVit/metric-collector/internal/agent/skeduller"
 )
 
+const endPoint = "http://localhost:8080/update/"
+const readTime int64 = 2
+const sendTime int64 = 10
+
 func main() {
 	if err := run(); err != nil {
 		panic(err)
@@ -13,6 +17,6 @@ func main() {
 
 func run() error {
 	cfg := config.GetConfig()
-	skeduller.StartTimer(cfg.PollInterval, cfg.ReportInterval, cfg.Host+"/update/")
+	skeduller.StartTimer(cfg.PollInterval, cfg.ReportInterval, "http://"+cfg.Host+"/update/")
 	return nil
 }
