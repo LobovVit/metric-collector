d3eeaccbee2322d9350aae8f4cf79dbc7381549d
vitaly <lobov.vitaliy@gmail.com>
2024-01-11 22:41:14 +0300

    iter5 fix по замечаниям ментора (+statictest)

--- internal/agent/skeduller/timer.go
+++ internal/agent/skeduller/timer.go
@@ -4,20 +4,18 @@ import (
 	"context"
 	"github.com/LobovVit/metric-collector/internal/agent/metrics"
 	"log"
-	"sync"
 	"time"
 )
 
 func StartTimer(ctx context.Context, readTime int64, sendTime int64, endPoint string) {
 	m := metrics.GetMetricStruct()
-	wg := sync.WaitGroup{}
+	//wg := sync.WaitGroup{}
 
 	readTicker := time.NewTicker(time.Second * time.Duration(readTime))
 	sendTicker := time.NewTicker(time.Second * time.Duration(sendTime))
 	defer sendTicker.Stop()
 	defer readTicker.Stop()
 
-	wg.Add(1)
 	for {
 		select {
 		case <-readTicker.C:
@@ -28,15 +26,12 @@ func StartTimer(ctx context.Context, readTime int64, sendTime int64, endPoint st
 			err := sendRequest(ctx, m, endPoint)
 			if err != nil {
 				log.Printf("err sendRequest %v\n", err)
-				wg.Done()
 				return
 			}
 			log.Printf("send\n")
 		case <-ctx.Done():
 			log.Printf("shutdown\n")
-			wg.Done()
 			return
 		}
 	}
-	wg.Wait()
 }
