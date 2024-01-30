5065b6a65e4dc5096ddca0860b1794d7637aa247
vitaly <lobov.vitaliy@gmail.com>
2024-01-31 00:50:35 +0300

    iter9 по замечаниям ментора

--- internal/server/server/middlewares/compress.go
+++ internal/server/server/middlewares/compress.go
@@ -1,11 +1,70 @@
 package middlewares
 
 import (
+	"compress/gzip"
+	"io"
 	"net/http"
 	"strings"
-
-	"github.com/LobovVit/metric-collector/internal/server/compress"
 )
+
+type compressWriter struct {
+	w  http.ResponseWriter
+	zw *gzip.Writer
+}
+
+func newCompressWriter(w http.ResponseWriter) *compressWriter {
+	return &compressWriter{
+		w:  w,
+		zw: gzip.NewWriter(w),
+	}
+}
+
+func (c *compressWriter) Header() http.Header {
+	return c.w.Header()
+}
+
+func (c *compressWriter) Write(p []byte) (int, error) {
+	return c.zw.Write(p)
+}
+
+func (c *compressWriter) WriteHeader(statusCode int) {
+	if statusCode < 300 {
+		c.w.Header().Set("Content-Encoding", "gzip")
+	}
+	c.w.WriteHeader(statusCode)
+}
+
+func (c *compressWriter) Close() error {
+	return c.zw.Close()
+}
+
+type compressReader struct {
+	r  io.ReadCloser
+	zr *gzip.Reader
+}
+
+func newCompressReader(r io.ReadCloser) (*compressReader, error) {
+	zr, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &compressReader{
+		r:  r,
+		zr: zr,
+	}, nil
+}
+
+func (c *compressReader) Read(p []byte) (n int, err error) {
+	return c.zr.Read(p)
+}
+
+func (c *compressReader) Close() error {
+	if err := c.r.Close(); err != nil {
+		return err
+	}
+	return c.zr.Close()
+}
 
 func WithCompress(h http.Handler) http.Handler {
 	logFn := func(w http.ResponseWriter, r *http.Request) {
@@ -13,14 +72,14 @@ func WithCompress(h http.Handler) http.Handler {
 		acceptEncoding := r.Header.Get("Accept-Encoding")
 		supportsGzip := strings.Contains(acceptEncoding, "gzip")
 		if supportsGzip {
-			cw := compress.NewCompressWriter(w)
+			cw := newCompressWriter(w)
 			ow = cw
 			defer cw.Close()
 		}
 		contentEncoding := r.Header.Get("Content-Encoding")
 		sendsGzip := strings.Contains(contentEncoding, "gzip")
 		if sendsGzip {
-			cr, err := compress.NewCompressReader(r.Body)
+			cr, err := newCompressReader(r.Body)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
