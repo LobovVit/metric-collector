f2d8c1209ae13bec7c212dbba5919ca000053581
vitaly <lobov.vitaliy@gmail.com>
2024-01-31 14:08:36 +0300

    iter9 по замечаниям ментора

--- internal/server/server/server_test.go
+++ internal/server/server/server_test.go
@@ -7,12 +7,16 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-var Ts *httptest.Server = Server_Run()
+var Ts = ServerRun()
 
-func Server_Run() *httptest.Server {
+func ServerRun() *httptest.Server {
 	mux := chi.NewRouter()
-	cfg, _ := config.GetConfig()
-	tst := New(cfg)
+	cfg := config.Config{Host: "localhost:8080", LogLevel: "info", StoreInterval: 100, FileStoragePath: "1.json", Restore: false}
+	tst := New(&cfg)
+	mux.Get("/", tst.allMetricsHandler)
+	mux.Post("/value/", tst.singleMetricJSONHandler)
+	mux.Get("/value/{type}/{name}", tst.singleMetricHandler)
 	mux.Post("/update/", tst.updateJSONHandler)
+	mux.Post("/update/{type}/{name}/{value}", tst.updateHandler)
 	return httptest.NewServer(mux)
 }
