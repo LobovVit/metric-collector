497a8b7d9e61b49fbd050bfadb06de055d334529
vitaly <lobov.vitaliy@gmail.com>
2024-02-03 05:27:49 +0300

    iter10

--- internal/server/server/dbPingHandler.go
+++ internal/server/server/dbPingHandler.go
@@ -2,18 +2,22 @@ package server
 
 import (
 	"net/http"
+
+	"github.com/LobovVit/metric-collector/pkg/logger"
 )
 
 func (a *Server) dbPingHandler(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/plain")
 	if a.dbCon == nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		logger.Log.Info("500 dbCon == nil")
 		return
 	}
 	err := a.dbCon.Ping()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
+		logger.Log.Info("500 dbCon.Ping err")
 		return
 	}
 	w.WriteHeader(http.StatusOK)
