717a61a4ea73bdd15dbb7cc66a288d363ca899e7
vitaly <lobov.vitaliy@gmail.com>
2024-02-08 14:46:26 +0300

    iter12

--- internal/server/server/updatebatchjsonandler.go
+++ internal/server/server/updatebatchjsonandler.go
@@ -6,14 +6,16 @@ import (
 	"net/http"
 
 	"github.com/LobovVit/metric-collector/internal/server/domain/metrics"
+	"github.com/LobovVit/metric-collector/pkg/logger"
+	"go.uber.org/zap"
 )
 
 func (a *Server) updateBatchJSONandler(w http.ResponseWriter, r *http.Request) {
 
 	var metricsBatch metrics.SlMetrics
 	var buf bytes.Buffer
-
 	_, err := buf.ReadFrom(r.Body)
+	logger.Log.Info("body", zap.String("q", buf.String()))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
