1e3c85d3ad659a7ef6ee7bf2a4593f425570b9bc
vitaly <lobov.vitaliy@gmail.com>
2024-03-03 23:07:10 +0300

    iter15

--- internal/agent/app/agent.go
+++ internal/agent/app/agent.go
@@ -16,6 +16,7 @@ import (
 	"github.com/LobovVit/metric-collector/pkg/signature"
 	"github.com/go-resty/resty/v2"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 type Agent struct {
@@ -30,11 +31,14 @@ func New(config *config.Config) *Agent {
 
 func (a *Agent) Run(ctx context.Context) error {
 	m := metrics.GetMetricStruct()
+	logger.Log.Info("Run")
+	var wg sync.WaitGroup
 	readTicker := time.NewTicker(time.Second * time.Duration(a.cfg.PollInterval))
 	defer readTicker.Stop()
-
 	//GetMetrics
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		for {
 			select {
 			case <-readTicker.C:
@@ -42,6 +46,7 @@ func (a *Agent) Run(ctx context.Context) error {
 				logger.Log.Info("Read")
 			case <-ctx.Done():
 				logger.Log.Info("Shutdown")
+				return
 			}
 		}
 	}()
@@ -49,30 +54,35 @@ func (a *Agent) Run(ctx context.Context) error {
 	//SendMetrics
 	sendTicker := time.NewTicker(time.Second * time.Duration(a.cfg.ReportInterval))
 	defer sendTicker.Stop()
-	sem := newSemaphore(a.cfg.RateLimit)
-	for {
-		select {
-		case <-sendTicker.C:
-			tmp := m.CounterExecMemStats.Load()
-			err := a.sendRequestWithRetry(ctx, m, sem)
-			m.CounterExecMemStats.Store(m.CounterExecMemStats.Load() - tmp)
-			if err != nil {
-				m.CounterExecMemStats.Store(tmp)
-				logger.Log.Error("Send request failed", zap.Error(err))
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-sendTicker.C:
+				tmp := m.CounterExecMemStats.Load()
+				err := a.sendRequestWithRetry(ctx, m)
+				m.CounterExecMemStats.Store(m.CounterExecMemStats.Load() - tmp)
+				if err != nil {
+					m.CounterExecMemStats.Store(tmp)
+					logger.Log.Error("Send request failed", zap.Error(err))
+				}
+				logger.Log.Info("Sent")
+			case <-ctx.Done():
+				logger.Log.Info("Shutdown")
+				return
 			}
-			logger.Log.Info("Sent")
-		case <-ctx.Done():
-			logger.Log.Info("Shutdown")
-			return nil
 		}
-	}
+	}()
+	wg.Wait()
+	return nil
 }
 
-func (a *Agent) sendRequestWithRetry(ctx context.Context, metrics *metrics.Metrics, sem *semaphore) error {
+func (a *Agent) sendRequestWithRetry(ctx context.Context, metrics *metrics.Metrics) error {
 	var err error
 	try := retry.New(3)
 	for {
-		err = a.sendRequest(ctx, metrics, sem)
+		err = a.sendRequest(ctx, metrics)
 		if err == nil || !try.Run() {
 			break
 		}
@@ -80,155 +90,154 @@ func (a *Agent) sendRequestWithRetry(ctx context.Context, metrics *metrics.Metri
 	return err
 }
 
-func (a *Agent) sendRequest(ctx context.Context, metrics *metrics.Metrics, sem *semaphore) error {
+func (a *Agent) sendRequest(ctx context.Context, metrics *metrics.Metrics) error {
 	metrics.RwMutex.RLock()
 	defer metrics.RwMutex.RUnlock()
 
 	switch a.cfg.ReportFormat {
 	case "json":
-		return a.sendRequestJSON(ctx, metrics, sem)
+		return a.sendRequestsJSON(ctx, metrics)
 	case "text":
-		return a.sendRequestText(ctx, metrics, sem)
+		return a.sendRequestsText(ctx, metrics)
 	case "batch":
-		return a.sendRequestBatchJSON(ctx, metrics, sem)
+		return a.sendRequestsBatchJSON(ctx, metrics)
 	default:
 		return fmt.Errorf("incorrect format")
 	}
 }
 
-func (a *Agent) sendRequestText(ctx context.Context, met *metrics.Metrics, sem *semaphore) error {
-	var ret error = nil
-	var wg sync.WaitGroup
+func (a *Agent) sendRequestsText(ctx context.Context, met *metrics.Metrics) error {
+	g := errgroup.Group{}
+	sem := newSemaphore(a.cfg.RateLimit)
 	for _, v := range met.Metrics {
-		wg.Add(1)
-		go func(met metrics.Metric, sem *semaphore) {
-			err := func() error {
-				var val string
-				sem.acquire()
-				switch met.MType {
-				case "gauge":
-					val = strconv.FormatFloat(*met.Value, 'f', 10, 64)
-				case "counter":
-					val = strconv.FormatInt(*met.Delta, 10)
-				}
-				_, err := a.client.R().
-					SetContext(ctx).
-					SetHeader("Content-Type", "text/plain").
-					Post(fmt.Sprintf("%v%v/%v/%v", a.cfg.Host, met.MType, met.ID, val))
-				if err != nil {
-
-					sem.release()
-					return fmt.Errorf("send: %w", err)
-				}
-				sem.release()
-				return nil
-			}()
-			if err != nil {
-				ret = fmt.Errorf("send request: %w", err)
-			}
-		}(v, sem)
-		wg.Done()
+		val := v
+		g.Go(func() error {
+			return a.sendSingleRequestText(ctx, val, sem)
+		})
 	}
-	wg.Wait()
-	return ret
+	if err := g.Wait(); err != nil {
+		logger.Log.Info("send", zap.Error(err))
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
 }
 
-func (a *Agent) sendRequestJSON(ctx context.Context, met *metrics.Metrics, sem *semaphore) error {
-	var ret error = nil
-	var wg sync.WaitGroup
-	for _, v := range met.Metrics {
-		wg.Add(1)
-		go func(val metrics.Metric, sem *semaphore) {
-			err := func() error {
-				sem.acquire()
-				metric, err := json.Marshal(val)
-				if err != nil {
-
-					return fmt.Errorf("marshal json: %w", err)
-				}
-				metric, err = compress.Compress(metric)
-				if err != nil {
-					return fmt.Errorf("compress json: %w", err)
-				}
-				if a.cfg.SigningKey != "" {
-					sign, err := signature.CreateSignature(metric, a.cfg.SigningKey)
-					if err != nil {
-						return fmt.Errorf("create signature: %w", err)
-					}
-					a.client.SetHeader("HashSHA256", fmt.Sprintf("%x", sign))
-				}
-				_, err = a.client.R().
-					SetContext(ctx).
-					SetHeader("Content-Type", "application/json").
-					SetHeader("Content-Encoding", "gzip").
-					SetBody(metric).
-					Post(a.cfg.Host)
-
-				if err != nil {
-					sem.release()
-					return fmt.Errorf("send json: %w", err)
-				}
-				sem.release()
-				return nil
-			}()
-			if err != nil {
-				ret = fmt.Errorf("send request: %w", err)
-			}
-		}(v, sem)
-		wg.Done()
+func (a *Agent) sendSingleRequestText(ctx context.Context, singleMetric metrics.Metric, sem *semaphore) error {
+	var val string
+	sem.acquire()
+	defer sem.release()
+	switch singleMetric.MType {
+	case "gauge":
+		val = strconv.FormatFloat(*singleMetric.Value, 'f', 10, 64)
+	case "counter":
+		val = strconv.FormatInt(*singleMetric.Delta, 10)
 	}
-	wg.Wait()
-	return ret
+	_, err := a.client.R().
+		SetContext(ctx).
+		SetHeader("Content-Type", "text/plain").
+		Post(fmt.Sprintf("%v%v/%v/%v", a.cfg.Host, singleMetric.MType, singleMetric.ID, val))
+	if err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
 }
 
-func (a *Agent) sendRequestBatchJSON(ctx context.Context, met *metrics.Metrics, sem *semaphore) error {
+func (a *Agent) sendRequestsJSON(ctx context.Context, met *metrics.Metrics) error {
+	g := errgroup.Group{}
+	sem := newSemaphore(a.cfg.RateLimit)
+	for _, v := range met.Metrics {
+		val := v
+		g.Go(func() error {
+			return a.sendSingleRequestJSON(ctx, val, sem)
+		})
+	}
+	if err := g.Wait(); err != nil {
+		logger.Log.Info("send", zap.Error(err))
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
+}
+
+func (a *Agent) sendSingleRequestJSON(ctx context.Context, singleMetric metrics.Metric, sem *semaphore) error {
+	sem.acquire()
+	defer sem.release()
+	metric, err := json.Marshal(singleMetric)
+	if err != nil {
+
+		return fmt.Errorf("marshal json: %w", err)
+	}
+	metric, err = compress.Compress(metric)
+	if err != nil {
+		return fmt.Errorf("compress json: %w", err)
+	}
+	if a.cfg.SigningKey != "" {
+		sign, err := signature.CreateSignature(metric, a.cfg.SigningKey)
+		if err != nil {
+			return fmt.Errorf("create signature: %w", err)
+		}
+		a.client.SetHeader("HashSHA256", fmt.Sprintf("%x", sign))
+	}
+	_, err = a.client.R().
+		SetContext(ctx).
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Content-Encoding", "gzip").
+		SetBody(metric).
+		Post(a.cfg.Host)
+
+	if err != nil {
+		return fmt.Errorf("send json: %w", err)
+	}
+	return nil
+}
+
+func (a *Agent) sendRequestsBatchJSON(ctx context.Context, met *metrics.Metrics) error {
 	var maxPart = len(met.Metrics) / a.cfg.MaxCntInBatch
-	var ret error = nil
-	var wg sync.WaitGroup
+	g := errgroup.Group{}
+	sem := newSemaphore(a.cfg.RateLimit)
 	for part := 0; part <= maxPart; part++ {
-		wg.Add(1)
-		go func(p int, sem *semaphore) {
-			err := func() error {
-				sem.acquire()
-				startPos := p * a.cfg.MaxCntInBatch
-				endPos := p*a.cfg.MaxCntInBatch + a.cfg.MaxCntInBatch
-				if endPos > len(met.Metrics) {
-
-					endPos = len(met.Metrics)
-				}
-				data, err := json.Marshal(met.Metrics[startPos:endPos])
-				if err != nil {
-					return fmt.Errorf("marshal json: %w", err)
-				}
-				data, err = compress.Compress(data)
-				if err != nil {
-					return fmt.Errorf("compress json: %w", err)
-				}
-				if a.cfg.SigningKey != "" {
-					sign, err := signature.CreateSignature(data, a.cfg.SigningKey)
-					if err != nil {
-						return fmt.Errorf("create signature: %w", err)
-					}
-					a.client.SetHeader("HashSHA256", fmt.Sprintf("%x", sign))
-				}
-				_, err = a.client.R().
-					SetContext(ctx).
-					SetHeader("Content-Type", "application/json").
-					SetHeader("Content-Encoding", "gzip").
-					SetBody(data).
-					Post(a.cfg.Host)
-				if err != nil {
-					sem.release()
-					return fmt.Errorf("send request json: %w", err)
-				}
-				sem.release()
-				return nil
-			}()
-			if err != nil {
-				ret = fmt.Errorf("send request: %w", err)
-			}
-		}(part, sem)
+		startPos := part * a.cfg.MaxCntInBatch
+		endPos := part*a.cfg.MaxCntInBatch + a.cfg.MaxCntInBatch
+		if endPos > len(met.Metrics) {
+			endPos = len(met.Metrics)
+		}
+		val := met.Metrics[startPos:endPos]
+		g.Go(func() error {
+			return a.sendSingleRequestBatchJSON(ctx, val, sem)
+		})
 	}
+	if err := g.Wait(); err != nil {
+		logger.Log.Info("send", zap.Error(err))
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
+}
 
-	return ret
+func (a *Agent) sendSingleRequestBatchJSON(ctx context.Context, singlePartMetric []metrics.Metric, sem *semaphore) error {
+	sem.acquire()
+	defer sem.release()
+	data, err := json.Marshal(singlePartMetric)
+	if err != nil {
+		return fmt.Errorf("marshal json: %w", err)
+	}
+	data, err = compress.Compress(data)
+	if err != nil {
+		return fmt.Errorf("compress json: %w", err)
+	}
+	if a.cfg.SigningKey != "" {
+		sign, err := signature.CreateSignature(data, a.cfg.SigningKey)
+		if err != nil {
+			return fmt.Errorf("create signature: %w", err)
+		}
+		a.client.SetHeader("HashSHA256", fmt.Sprintf("%x", sign))
+	}
+	_, err = a.client.R().
+		SetContext(ctx).
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Content-Encoding", "gzip").
+		SetBody(data).
+		Post(a.cfg.Host)
+	if err != nil {
+		return fmt.Errorf("send request json: %w", err)
+	}
+	return nil
 }
